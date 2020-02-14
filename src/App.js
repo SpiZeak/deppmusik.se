6e9ee969ace39056b087c1ebee8ae98f6c9ae4b5
Max Trewhitt <max.trewhitt@osolo.se>
2020-02-14 18:41:27 +0100

    Working example

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React, { Component } from "react";
-import * as $ from "jquery";
-import { authEndpoint, clientId, redirectUri, scopes } from "./config";
-import hash from "./hash";
-import Player from "./Player";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import * as $ from 'jquery';
+import { authEndpoint, clientId, redirectUri, scopes } from './config';
+import hash from './hash';
+import Player from './Player';
+import './App.css';
+
+let player;
+
+/* global Spotify */
 
 class App extends Component {
   constructor() {
     super();
+
     this.state = {
       token: null,
       item: {
         album: {
-          images: [{ url: "" }]
+          images: [{ url: '' }],
         },
-        name: "",
-        artists: [{ name: "" }],
-        duration_ms: 0
+        name: '',
+        artists: [{ name: '' }],
+        duration_ms: 0,
       },
-      is_playing: "Paused",
-      progress_ms: 0
+      is_playing: 'Paused',
+      progress_ms: 0,
     };
     this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
   }
+
   componentDidMount() {
     // Set token
     let _token = hash.access_token;
@@ -31,45 +36,90 @@ class App extends Component {
     if (_token) {
       // Set token
       this.setState({
-        token: _token
+        token: _token,
       });
       this.getCurrentlyPlaying(_token);
+    } else {
+      window.location = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+        '%20',
+      )}&response_type=token&show_dialog=true`;
     }
+
+    window.onSpotifyPlayerAPIReady = () => {
+      player = new Spotify.Player({
+        name: 'Web Playback SDK Template',
+        getOAuthToken: cb => {
+          cb(_token);
+        },
+      });
+
+      // Error handling
+      player.on('initialization_error', e => console.error(e));
+      player.on('authentication_error', e => console.error(e));
+      player.on('account_error', e => console.error(e));
+      player.on('playback_error', e => console.error(e));
+
+      // Playback status updates
+      player.on('player_state_changed', state => {
+        $('#current-track').attr(
+          'src',
+          state.track_window.current_track.album.images[0].url,
+        );
+        $('#current-track-name').text(state.track_window.current_track.name);
+      });
+
+      // Ready
+      player.on('ready', data => {
+        console.log('Ready with Device ID', data.device_id);
+
+        // Play a track using our new device ID
+        this.play(data.device_id, this.state.token);
+      });
+
+      //Connect to the player!
+      player.connect();
+    };
   }
+
+  play = (device_id, token) => {
+    $.ajax({
+      url: 'https://api.spotify.com/v1/me/player/play?device_id=' + device_id,
+      type: 'PUT',
+      data: '{"uris": ["spotify:track:5ya2gsaIhTkAuWYEMB0nw5"]}',
+      beforeSend: xhr => {
+        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+      },
+      success: data => {
+        this.getCurrentlyPlaying();
+      },
+    });
+  };
 
   getCurrentlyPlaying(token) {
     // Make a call using the token
     $.ajax({
-      url: "https://api.spotify.com/v1/me/player",
-      type: "GET",
+      url: 'https://api.spotify.com/v1/me/player',
+      type: 'GET',
       beforeSend: xhr => {
-        xhr.setRequestHeader("Authorization", "Bearer " + token);
+        xhr.setRequestHeader('Authorization', 'Bearer ' + token);
       },
       success: data => {
-        this.setState({
-          item: data.item,
-          is_playing: data.is_playing,
-          progress_ms: data.progress_ms
-        });
-      }
+        console.log(data);
+        if (data) {
+          this.setState({
+            item: data.item,
+            is_playing: data.is_playing,
+            progress_ms: data.progress_ms,
+          });
+        }
+      },
     });
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          {!this.state.token && (
-            <a
-              className="btn btn--loginApp-link"
-              href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                "%20"
-              )}&response_type=token&show_dialog=true`}
-            >
-              Login to Spotify
-            </a>
-          )}
+      <div className='App'>
+        <header className='App-header'>
           {this.state.token && (
             <Player
               item={this.state.item}
